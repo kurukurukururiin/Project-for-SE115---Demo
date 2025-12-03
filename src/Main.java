670b7f824e32e99c059d7384c18bd07258991539
kurukurukururiin <egekuru2006@gmail.com>
2025-12-03 22:58:34 +0300

    Classes has been created and I've thinked what to do for the project.

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MONTHS = 12;
+    static final int DAYS = 28;
+    static final int COMMS = 5;
+    static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
+    static String[] months = {"January","February","March","April","May","June",
+            "July","August","September","October","November","December"};
+
+
+    // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
+    public static void loadData() {
+        Scanner sc = new Scanner(new File("/Data_Files/April.txt"));
+        System.out.println(sc.next());
+    }
+
+    // ======== 10 REQUIRED METHODS (Students fill these) ========
+
+    public static String mostProfitableCommodityInMonth(int month) {
+        if (month>11 || month<0)
+            return "INVALID_MONTH";
+        return "7";
+
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+        return 1234;
+    }
+
+    public static int commodityProfitInRange(String commodity, int from, int to) {
+        return 1234;
+    }
+
+    public static int bestDayOfMonth(int month) {
+        return 1234;
+    }
+
+    public static String bestMonthForCommodity(String comm) {
+        return "DUMMY";
+    }
+
+    public static int consecutiveLossDays(String comm) {
+        return 1234;
+    }
+
+    public static int daysAboveThreshold(String comm, int threshold) {
+        return 1234;
+    }
+
+    public static int biggestDailySwing(int month) {
+        return 1234;
+    }
+
+    public static String compareTwoCommodities(String c1, String c2) {
+        return "DUMMY is better by 1234";
+    }
+
+    public static String bestWeekOfMonth(int month) {
+        return "DUMMY";
+    }
+
+    public static void main(String[] args) {
+        loadData();
+        System.out.println("Data loaded – ready for queries");
+    }
+}
