8cdb69d7e761772ca589d7910d5ce25aba4bbc23
kurukurukururiin <egekuru2006@gmail.com>
2025-12-16 13:25:19 +0300

    I've implemented totalProfitInRange() method by taking month and day values and summing the profit of that day in that month to a sum integer.
    I've implemented commodityProfitInRange method by taking the requested values and adding the profit of each day after the "from" value to "to" value for given month.

--- src/Main.java
+++ src/Main.java
@@ -70,11 +70,39 @@ public class Main {
     }
 
     public static int totalProfitOnDay(int month, int day) {
-        return 1234;
+        if (month > 11 || month < 0 || day < 0 || day > 27 ) {
+            return -99999; //error
+        }
+
+        int totalProfitOnDay = 0;
+        for (int i = 0 ; i < 5 ; i++ ) {
+            totalProfitOnDay += data[month][day][i];
+        }
+        return totalProfitOnDay;
     }
 
     public static int commodityProfitInRange(String commodity, int from, int to) {
-        return 1234;
+        if ( commodity != "Gold" && commodity != "Wheat" && commodity != "Oil" && commodity != "Silver" && commodity != "Copper" ) {
+            if (to > 27 || from < 0 || to < 0 || from > to) {
+                return -99999;
+            }
+        }
+        int sum = 0;
+        int month = 0;
+        if (month > 11 || month < 0) {
+            return -99999;
+        }
+        int a = 0;
+        for(int i = 0 ; i < commodities.length ; i++) {
+           if (commodities[i] == commodity) {
+               a = i;
+               break;
+           }
+        }
+        for (; from < to ; from++) {
+            sum += data[month][from][a];
+        }
+        return sum;
     }
 
     public static int bestDayOfMonth(int month) {
@@ -107,8 +135,10 @@ public class Main {
 
     public static void main(String[] args) {
         loadData();
+        System.out.println("Data loaded – ready for queries");
         System.out.println(mostProfitableCommodityInMonth(6));
         System.out.println(data[0][14][0]);
-        System.out.println("Data loaded – ready for queries");
+        System.out.println(totalProfitOnDay(0,27));
+        System.out.println(commodityProfitInRange("Gold",0,1));
     }
 }
